9f52074139d5bca7ef17f6822002d9316ecce905
Enoquef <nettolinhares18@gmail.com>
2020-08-07 11:11:39 -0300

    adicionando funcionalidade do botao + novo horario com JS

---
+++ scripts/addField.js
@@ -0,0 +1,29 @@
+// Procurar o botao
+document.querySelector('#add-time')
+// quando clicar no botao
+.addEventListener('click', cloneField)
+// executar uma acao
+
+function cloneField() {
+    //duplicar os campos. que campos?
+    const newFieldCotainer = document.querySelector('.schedule-item').cloneNode(true) 
+    
+    // pegar os campos: que campos ?
+    const  fields = newFieldCotainer.querySelectorAll('input')
+
+    // para cada campo, limpar
+    fields.forEach(function(field) {
+        // pegar o field atual e limpa ele
+        field.value = ""
+        
+    })
+    
+    
+    //colocar na pagina: onde ?
+    document.querySelector('#schedule-items').appendChild(newFieldCotainer)
+}
+
+
+
+
+
